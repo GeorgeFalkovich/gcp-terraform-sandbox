631d3588492780eb1c6ea721906e69b894b9cd07
george.falkovich@comm-it.cloud <george.falkovich@comm-it.cloud>
2025-05-26 21:37:27 +0300

    before destroy" ;

--- cloudsql.tf
+++ cloudsql.tf
@@ -1,39 +1,39 @@
 
-resource "google_sql_database_instance" "example" {
-  name                = "example-sql-instance"
-  database_version    = "MYSQL_8_0"
-  region              = "us-central1"
-  deletion_protection = false
+# resource "google_sql_database_instance" "example" {
+#   name                = "example-sql-instance"
+#   database_version    = "MYSQL_8_0"
+#   region              = "us-central1"
+#   deletion_protection = false
 
-  settings {
-    tier = "db-n1-standard-1" # Change to a suitable tier for your needs
+#   settings {
+#     tier = "db-n1-standard-1" # Change to a suitable tier for your needs
 
-    backup_configuration {
-      enabled = true
-    }
+#     backup_configuration {
+#       enabled = true
+#     }
 
-    ip_configuration {
-      ipv4_enabled                                  = false
-      private_network                               = "projects/georgef-sandbox/global/networks/default"
-      enable_private_path_for_google_cloud_services = true
-    }
-  }
-}
+#     ip_configuration {
+#       ipv4_enabled                                  = false
+#       private_network                               = "projects/georgef-sandbox/global/networks/default"
+#       enable_private_path_for_google_cloud_services = true
+#     }
+#   }
+# }
 
-resource "google_sql_database" "example_db" {
-  name     = "exampledb"
-  instance = google_sql_database_instance.example.name
-}
+# resource "google_sql_database" "example_db" {
+#   name     = "exampledb"
+#   instance = google_sql_database_instance.example.name
+# }
 
-resource "google_sql_user" "example_user" {
-  name     = "exampleuser"
-  instance = google_sql_database_instance.example.name
-  password = "examplepassword" # Replace with a strong password
-}
+# resource "google_sql_user" "example_user" {
+#   name     = "exampleuser"
+#   instance = google_sql_database_instance.example.name
+#   password = "examplepassword" # Replace with a strong password
+# }
 
-output "instance_connection_name" {
-  value = google_sql_database_instance.example.connection_name
-}
+# output "instance_connection_name" {
+#   value = google_sql_database_instance.example.connection_name
+# }
 
 
 
